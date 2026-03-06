30de4a5a6d120f64e16e35d001b9d4b493ef568e
Lucas Cardozo <contactocardozolucas@gmail.com>
2026-03-06 00:25:52 -0300

    Interfaz login y funcionalidad btn

---
+++ app/src/main/java/frgp/utn/edu/ar/appmobile/LoginActivity.java
@@ -0,0 +1,38 @@
+package frgp.utn.edu.ar.appmobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void eventoBtnIniciarSesion(View view) {
+        intent = new Intent(getApplicationContext(), ConfigParamActivity.class);
+        startActivity(intent);
+    }
+
+    public void eventoBtnRegistrarse(View view) {
+        intent = new Intent(getApplicationContext(), RegistroActivity.class);
+        startActivity(intent);
+    }
+
+}
